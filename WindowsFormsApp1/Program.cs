0efd6bcb8c5b5f36d373372ef201bea19b2c41fd
InferKing <viacinoleg2012@gmail.com>
2022-03-30 08:28:28 +0500

    master

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace WindowsFormsApp1
 {
@@ -18,7 +21,65 @@ namespace WindowsFormsApp1
             Application.SetCompatibleTextRenderingDefault(false);
             Form2 f = new Form2();
             f.StartPosition = FormStartPosition.CenterScreen;
+            Data d = new Data();
             Application.Run(f);
         }
+
     }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string Score { get; set; }
+
+        public Person(string name, string surname, string patr, string score)
+        {
+            Name = name;
+            Surname = surname;
+            Patronymic = patr;
+            Score = score;
+        }
+    }
+
+    class Data
+    {
+        private string filename = "/data.dat";
+
+        public static string jsonString;
+        public void SaveData()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.StartupPath + filename, FileMode.Create);
+            bf.Serialize(file, jsonString);
+            file.Close();
+        }
+
+        public void LoadData()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            if (File.Exists(Application.StartupPath + filename))
+            {
+                FileStream file = File.Open(Application.StartupPath + filename, FileMode.Open);
+                jsonString = (string)bf.Deserialize(file);
+                file.Close();
+            }
+            else
+            {
+                FileStream file = File.Open(Application.StartupPath + filename, FileMode.CreateNew);
+                file.Close();
+            }
+        }
+    }
+
+    class Question
+    {
+        public string TypeOfQuestion { get; set; }
+        public string Answer { get; set; }
+
+    }
+
+
+
 }
